d7d35780b9bdcff0fd9412ee30cd802de3614af5
hungluu2405 <luukhanhhung.2405@gmail.com>
2025-10-04 19:54:33 +0700

    Add .gitkeep files to show package structure

---
+++ src/main/java/controller/HelloServlet.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Gửi dữ liệu sang JSP
+        request.setAttribute("message", "Xin chào từ Servlet!");
+        request.getRequestDispatcher("hello.jsp").forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello JSP</title>
+</head>
+<body>
+    <h2>Test Servlet & JSP hoạt động!</h2>
+    <p>${message}</p>
+</body>
+</html>
