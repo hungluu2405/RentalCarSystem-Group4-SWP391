54f8ec3053990e3a9fbcf83972d9e0bf4d21cbce
anvvhe190784 <vananvu249@gmail.com>
2025-10-12 02:59:27 +0700

    Update Google OAuth credentials and remove Tomcat config
    Replaced Google OAuth client ID, secret, and redirect URI in Constants.java to reflect new credentials and port. Deleted the Tomcat 10.1.40 run configuration file. Minor formatting change in pom.xml.

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,9 +1,9 @@
 package util;
 
 public class Constants {
-    public static String GOOGLE_CLIENT_ID = "966214955481-9buoi44ftmdlp5l61l07fd973kesm9rh.apps.googleusercontent.com";
-    public static String GOOGLE_CLIENT_SECRET = "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";
-    public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/CarRentalSystem/login-google-handler";
+    public static String GOOGLE_CLIENT_ID = "966214955481-gp7fbfm6tml7iuektmqc6qo2h4jopjdo.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/CarRentalSystem/login-google-handler";
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
     public static String GOOGLE_GRANT_TYPE = "authorization_code";
