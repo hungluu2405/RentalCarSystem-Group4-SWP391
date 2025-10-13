aa15f8cd65c18d7747a5a0ce3dd2710f372bce95
hungluu2405 <luukhanhhung.2405@gmail.com>
2025-10-13 18:57:25 +0700

    delete unusage class

--- src/main/java/controller/common/RootRedirectServlet.java
+++ src/main/java/controller/common/RootRedirectServlet.java
@@ -1,23 +1,23 @@
-package controller.common;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebServlet(urlPatterns = {"", "/"})
-public class RootRedirectServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String contextPath = req.getContextPath();
-        resp.sendRedirect(contextPath + "/home");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
-    }
-}
+//package controller.common;
+//
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+////@WebServlet(urlPatterns = {"", "/"})
+//public class RootRedirectServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String contextPath = req.getContextPath();
+//        resp.sendRedirect(contextPath + "/home");
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        doGet(req, resp);
+//    }
+//}
