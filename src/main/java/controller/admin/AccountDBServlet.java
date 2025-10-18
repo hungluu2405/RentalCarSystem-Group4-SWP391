c99895040bd51ff07f2e2710cb29add40ae91e12
hungvuo5-admin <nguyenvuhung1102@gmail.com>
2025-10-18 22:57:46 +0700

    Update ui,iconCard

--- src/main/java/controller/admin/AccountDBServlet.java
+++ src/main/java/controller/admin/AccountDBServlet.java
@@ -1,6 +1,6 @@
 package controller.admin;
 
-import dao.implement.UserDAO;
+import dao.implement.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +8,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+
+import model.Booking;
+import model.Car;
 import model.User;
 
 @WebServlet(name = "AccountDashBoard", urlPatterns = {"/accountDB"})
@@ -18,11 +21,19 @@ public class AccountDBServlet extends HttpServlet {
             throws ServletException, IOException {
 
         UserDAO userDAO = new UserDAO();
+        CarDAO carDAO = new CarDAO();
+        BookingDAO bookingDAO = new BookingDAO();
+
         List<User> listU = userDAO.getAllUsersForAdmin();
+
         int totalUsers = userDAO.countAllUsers();
+        int totalCars = carDAO.countAllCars();
+        int totalBookings = bookingDAO.countAllBookings();
 
         request.setAttribute("listU", listU);
         request.setAttribute("totalUsers",totalUsers);
+        request.setAttribute("totalCars",totalCars);
+        request.setAttribute("totalBookings",totalBookings);
 
         request.getRequestDispatcher("/view/admin/accountdashboard.jsp").forward(request, response);
     }
--- src/main/java/controller/admin/BookingDBServlet.java
+++ src/main/java/controller/admin/BookingDBServlet.java
@@ -1,6 +1,6 @@
 package controller.admin;
 
-import dao.implement.BookingDAO;
+import dao.implement.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -20,14 +20,24 @@ public class BookingDBServlet extends HttpServlet {
             throws ServletException, IOException {
 
 
+        UserDAO userDAO = new UserDAO();
+        CarDAO carDAO = new CarDAO();
         BookingDAO bookingDAO = new BookingDAO();
+
+        int totalUsers = userDAO.countAllUsers();
+        int totalCars = carDAO.countAllCars();
+        int totalBookings = bookingDAO.countAllBookings();
         List<Booking> listB = null;
         try {
             listB = bookingDAO.getAllBookings();
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         request.setAttribute("listB", listB);
+        request.setAttribute("totalUsers",totalUsers);
+        request.setAttribute("totalCars",totalCars);
+        request.setAttribute("totalBookings",totalBookings);
 
         request.getRequestDispatcher("/view/admin/BookingDashboard.jsp").forward(request, response);
     }
