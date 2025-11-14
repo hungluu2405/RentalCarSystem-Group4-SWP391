108f82055400f41dc438b509c4ba23242916270d
hungvuo5-admin <nguyenvuhung1102@gmail.com>
2025-11-14 17:47:29 +0700

    Update vietnamese

--- src/main/webapp/js/demo/datatables-demo.js
+++ src/main/webapp/js/demo/datatables-demo.js
@@ -1,8 +1,18 @@
+// // Call the dataTables jQuery plugin
+// $(document).ready(function() {
+//   $('#dataTable').DataTable();
+// });
+//
+// $('#dataTable').DataTable( {
+//   colReorder: true
+// } );
+
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+    $('#dataTable').DataTable({
+        colReorder: true,
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.13.7/i18n/vi.json"
+        }
+    });
 });
-
-$('#dataTable').DataTable( {
-  colReorder: true
-} );
